2c8d4ffa0c80eee035884e7a5d2076da0984e7b2
Naufal Azim <naufal.azim@outlook.com>
2022-04-04 15:45:48 +0800

    1.0v

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import './index.css'
+
 
 function App() {
+
+  //state
+  const[weight, setWeight] = useState(0)
+  const[height, setHeight] = useState(0)
+  const[bmi, setBmi] = useState('')
+  const[message, setMessage] = useState('')
+
+  let imgSrc = ''
+
+  //do calculation:
+  let calcBmi = (event) => {
+
+    //prevent submit:
+    event.preventDefault()
+
+    if(weight === 0 || height === 0) {
+      alert('Please enter your weight and height')
+    }else {
+      //formula: weight * height^2
+      let bmi = ((weight) / (height*height))
+      setBmi(bmi.toFixed(2))
+
+      //Logic for message:
+      if(bmi < 18.5) {
+        setMessage("Underweight")
+      }else if(bmi >=18.5 && bmi < 25) {
+        setMessage('You are normal')
+      }else if(bmi>=25 && bmi <= 30){
+        setMessage('Overweight')
+      }else {
+        setMessage('Gemok sangat obes woi!')
+      }
+
+    }
+  }
+
+
+  //reset
+  let reload = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+
+        <div>
+          <label>Weight (kg)</label>
+          <input value={weight} onChange={(event) => setWeight(event.target.value)} />
+        </div>
+
+        <div>
+          <label>Height (m)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value)}  />
+        </div>
+
+        <button className="btn" type="submit">Submit</button>
+        <button className="btn btn-outline" type="submit" onClick={reload}>Reload</button>
+        </form>
+
+        <div className="center">
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className="img-container">
+          <img src={imgSrc} alt="" />
+        </div>
+
+      </div>
     </div>
   );
 }
